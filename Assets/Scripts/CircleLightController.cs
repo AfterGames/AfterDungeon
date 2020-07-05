3922d552512b255001a60c136a80022e3fdf190f
rurugy15 <47134003+rurugy15@users.noreply.github.com>
2020-07-06 00:20:16 +0900

    플레이어 캐릭터 주변만 밝게 할 수 있도록 기능 추가

---
+++ Assets/Scripts/CircleLightController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleLightController : MonoBehaviour
+{
+    public float Brightness;
+    public float lightRadius;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        GetComponent<Renderer>().material.SetFloat("_Radius", lightRadius);
+        GetComponent<Renderer>().material.SetFloat("_Bright", Brightness);
+        GetComponent<Renderer>().material.SetFloat("_CenterX", transform.position.x);
+        GetComponent<Renderer>().material.SetFloat("_CenterY", transform.position.y);
+        transform.localScale = new Vector3(2 * lightRadius, 2 * lightRadius, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // GetComponent<Renderer>().material.SetFloat("_CenterX", transform.position.x);
+        // GetComponent<Renderer>().material.SetFloat("_CenterY", transform.position.y);
+    }
+}
---
+++ Assets/Scripts/PlayerBacklight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBacklight : CircleLightController
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(2 * lightRadius * transform.parent.localScale.x, 2 * lightRadius, 1);
+        GetComponent<Renderer>().material.SetFloat("_CenterX", transform.position.x);
+        GetComponent<Renderer>().material.SetFloat("_CenterY", transform.position.y);
+
+    }
+
+}
