a05a0afe553730d8e6d483669bd1e85bc3e4a9b5
PKR <345t@naver.com>
2020-07-09 00:12:32 +0900

    전등 실험중

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    private new Light light;
+    float originalIntensity;
+    private void Awake()
+    {
+        light = GetComponent<Light>();
+        originalIntensity = light.intensity;
+    }
+
+    float period = 5f;
+    float elapsedTime = 0;
+    float amplitude = 0.25f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(elapsedTime >= period)
+        {
+            elapsedTime = 0;
+            period = Random.Range(4f, 6f);
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+            light.intensity = originalIntensity * (1 + amplitude * Mathf.Sin(2 * Mathf.PI* elapsedTime / period));
+        }
+    }
+}
