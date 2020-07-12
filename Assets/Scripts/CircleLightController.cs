0a6de9497ece3c930da76bcdbc25bcc9f5899708
rurugy15 <47134003+rurugy15@users.noreply.github.com>
2020-07-12 13:27:52 +0900

    조명의 빛 범위 바뀌는 효과 추가

--- Assets/Scripts/CircleLightController.cs
+++ Assets/Scripts/CircleLightController.cs
@@ -6,6 +6,11 @@ public class CircleLightController : MonoBehaviour
 {
     public float Brightness;
     public float lightRadius;
+    [SerializeField] private float curRadius;
+    [SerializeField] private bool isBlink;
+    [SerializeField] private float blinkRange;
+
+    private int BlinkDirection = 1;
 
     // Start is called before the first frame update
     protected void Start()
@@ -15,12 +20,25 @@ public class CircleLightController : MonoBehaviour
         GetComponent<Renderer>().material.SetFloat("_CenterX", transform.position.x);
         GetComponent<Renderer>().material.SetFloat("_CenterY", transform.position.y);
         transform.localScale = new Vector3(2 * lightRadius, 2 * lightRadius, 1);
+        curRadius = lightRadius;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isBlink)
+            Blink();
         // GetComponent<Renderer>().material.SetFloat("_CenterX", transform.position.x);
         // GetComponent<Renderer>().material.SetFloat("_CenterY", transform.position.y);
+    }
+
+    protected void Blink()
+    {
+        curRadius += BlinkDirection * 0.004f;
+        if (curRadius > lightRadius + blinkRange)
+            BlinkDirection = -1;
+        else if (curRadius < lightRadius - blinkRange)
+            BlinkDirection = 1;
+        GetComponent<Renderer>().material.SetFloat("_Radius", curRadius);
     }
 }
