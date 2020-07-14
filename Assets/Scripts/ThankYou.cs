7822ec54bf2fac4fdf4c9c2a2829c64676c6f7eb
PKR <345t@naver.com>
2020-07-14 09:01:00 +0900

    WlsWlsakr
    게임이 끝나면 감사 인사를 함

---
+++ Assets/Scripts/ThankYou.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ThankYou : MonoBehaviour
+{
+    public void Thankyou()
+    {
+        StartCoroutine(aaa());
+    }
+
+    IEnumerator aaa()
+    {
+
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Main");
+    }
+}
