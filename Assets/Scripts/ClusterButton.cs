5499d8d04e35ebce9e6b70e076af2155a5b2f792
PKR <345t@naver.com>
2020-07-29 19:42:27 +0900

    버튼 카르텔 구현

---
+++ Assets/Scripts/ClusterButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClusterButton : ContactArrow
+{
+    public ClusterButtonGate gate;
+    public bool turnedOn; 
+    private bool TurnedOn
+    {
+        get { return turnedOn; }
+        set
+        {
+            turnedOn = value;
+            spr.sprite = value ? onImage : offImage;
+        }
+    }
+
+    public SpriteRenderer spr;
+    public Sprite onImage;
+    public Sprite offImage;
+    private void Awake()
+    {
+        //mover
+    }
+
+    public override void OnLodgingEnterAction(GameObject arrow)
+    {
+        TurnedOn = true;
+        gate.Check();
+    }
+    public override void OnLodgingStayAction(GameObject arrow)
+    {
+    }
+    public override void OnLodgingExitAction(GameObject arrow)
+    {
+    }
+
+    private void Update()
+    {
+        if(TurnedOn)
+        {
+            if (PlayerMovement_Kinematic.instance.IsGrounded)
+                TurnedOn = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ClusterButtonGate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterButtonGate : MonoBehaviour
+{
+    public List<ClusterButton> buttons = new List<ClusterButton>();
+    //List<GameObject> platforms = new List<GameObject>();
+
+    private void Awake()
+    {
+        for(int i = 0; i < buttons.Count; i++)
+        {
+            buttons[i].gate = this;
+        }
+
+    }
+
+    public void Check()
+    {
+        Debug.Log("check");
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            if (!buttons[i].turnedOn) return;
+        }
+        Open();
+    }
+
+    private void Open()
+    {
+        Debug.Log("open");
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
