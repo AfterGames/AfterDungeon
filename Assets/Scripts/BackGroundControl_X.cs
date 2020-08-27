13170b3e2d9d0ee342bb9f9c9411744d7e00dd30
PKR <345t@naver.com>
2020-08-28 00:52:59 +0900

    test 씬 카메라 이동 체계 수정

---
+++ Assets/Scripts/BackGroundControl_X.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundControl_X : MonoBehaviour
+{
+    public Vector3 playerOriginalPos;
+    public Vector3 originalPos;
+    public float ratio;
+    private void Awake()
+    {
+        playerOriginalPos = Player.instance.transform.position;
+        originalPos = transform.position;
+    }
+    private void Update()
+    {
+        transform.position = originalPos + Vector3.right * (Player.instance.transform.position.x - playerOriginalPos.x) * ratio;
+    }
+}
---
+++ Assets/Scripts/Camera_PlayerFollower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_PlayerFollower : MonoBehaviour
+{
+    public GameObject mid;
+    public float ratio;
+
+    void Update()
+    {
+        var v = transform.position;
+        v.x = Player.instance.transform.position.x + 6.2f;
+        var transition = v - transform.position;
+        mid.transform.localPosition -= transition * ratio;
+        transform.position = v;
+    }
+}
