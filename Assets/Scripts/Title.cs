253df34f1285354c355fb6141f0222d868ac891d
PKR <345t@naver.com>
2020-10-07 22:41:31 +0900

    BIC demo의 주인공 후광 밝기 수정, 타이틀 화면의 시작 버트과 크레딧 활성화

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
     private bool started = false;
     public PlayableDirector upwards;
     public TimelineAsset tla;
-    void Update()
-    {
-        if(Input.anyKeyDown && !started)
-        {
-            started = true;
-            upwards.Play(tla);
-        }
-    }
+    //void Update()
+    //{
+    //    if(Input.anyKeyDown && !started)
+    //    {
+    //        started = true;
+    //        upwards.Play(tla);
+    //    }
+    //}
     public void Quit()
     {
         Application.Quit();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("BIC_Demo");
     }
 }
