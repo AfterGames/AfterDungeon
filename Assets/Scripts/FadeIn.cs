bd6d724a1c15979b644bb0f06eec090f4e2e3d95
PKR <345t@naver.com>
2020-10-08 00:29:43 +0900

    씬 전환 시 fade out in 구현

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    SpriteRenderer sr;
+    int tick = 100;
+    int initialTick;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        initialTick = tick;
+    }
+
+
+    private void Update()
+    {
+        if(--tick > 0)
+        {
+            sr.color = new Color(0, 0, 0, ((float)tick / initialTick));
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
