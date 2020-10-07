c4d46925f091b1a82c26f0db14633d5845083071
PKR <345t@naver.com>
2020-10-07 23:26:49 +0900

    타이틀 화면에 커서 올림 효과 적용, 주인공 후광 밝기 감소

---
+++ Assets/Scripts/BoldChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BoldChanger : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
+{
+    public Text text;
+    public Font normalFont;
+    public Font boldFont;
+
+    private void Awake()
+    {
+        text = transform.GetComponentInChildren<Text>();
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        text.font = boldFont;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        text.font = normalFont;
+    }
+}
