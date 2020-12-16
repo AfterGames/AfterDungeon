6c4a6c9a6010a4935b521b98831ffa734fec5b7d
PKR <345t@naver.com>
2020-12-16 21:39:07 +0900

    원석 코드를 짬
    제 컴퓨터가 요즘 유니티 에디터를 켜면 뻗는 관계로 본격적인 적용은 다른 방법을 찾아서 하겠습니다

---
+++ Assets/Scripts/GemStone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemStone : MonoBehaviour
+{
+    public GemStoneManager mgrPrefab;
+
+    private void Start()
+    {
+        if (GemStoneManager.instance == null)
+            Instantiate(mgrPrefab);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GemStoneManager.instance.Collect();
+            gameObject.SetActive(false);
+            GemStoneManager.instance.Reset();
+        }
+    }
+}
---
+++ Assets/Scripts/GemStoneReset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemStoneReset : ResetableObject
+{
+    private GemStone gs;
+
+    private void Awake()
+    {
+        gs = GetComponent<GemStone>();
+    }
+
+    public override void Reset()
+    {
+        gameObject.SetActive(true);
+        GemStoneManager.instance.Reset();
+    }
+}
