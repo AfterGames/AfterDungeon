e8fd3496737c078ea6968714c45a9e2c32f7bf02
pkr <345t@naver.com>
2021-01-19 14:15:03 +0900

    늘어나는 적 구현

---
+++ Assets/Scripts/Stretcher.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stretcher : MonoBehaviour
+{
+    public StretcherManager mgrPrefab;
+    public bool stretched;
+    private bool stretchedAtStart;
+    private Transform child;
+
+    private AudioSource audioSource;
+    public AudioClip stretchSound;
+    public AudioClip shrinkSound;
+
+    public enum Direction { up, down, left, right}
+    public Direction direction;
+    private void Awake()
+    {
+        stretchedAtStart = stretched;
+        audioSource = GetComponent<AudioSource>();
+        child = transform.GetChild(0);
+        switch(direction)
+        {
+            case Direction.up:
+                transform.up = Vector3.up;
+                break;
+            case Direction.down:
+                transform.up = Vector3.down;
+                break;
+            case Direction.left:
+                transform.up = Vector3.left;
+                break;
+            case Direction.right:
+                transform.up = Vector3.right;
+                break;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (StretcherManager.instance == null)
+            StretcherManager.instance = Instantiate(mgrPrefab);
+        if (!StretcherManager.instance.stretchers.Contains(this))
+            StretcherManager.instance.stretchers.Add(this);
+    }
+
+    public void Move(float x)
+    {
+        if(stretched)
+        {
+            x = 1f - 0.75f * x;
+            child.localScale = new Vector3(1, x, 1);
+        }
+        else
+        {
+            x = 0.25f + 0.75f * x;
+            child.localScale = new Vector3(1, x, 1);
+        }
+        child.localPosition = new Vector3(0, 8 * x - 2, 0);
+    }
+
+    public void Reset()
+    {
+        stretched = stretchedAtStart;
+    }
+
+    public void Play()
+    {
+        if (shrinkSound == null || stretchSound == null) return;
+        audioSource.clip = stretched ? shrinkSound : stretchSound;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/StretcherManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretcherManager : MonoBehaviour
+{
+    public static StretcherManager instance;
+    public List<Stretcher> stretchers = new List<Stretcher>();
+
+    public float moveTime;
+    public float pauseTime;
+
+    enum State { moving, pause }
+    State currentState = State.pause;
+
+    private float elapsedTime = 0;
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (currentState == State.moving)
+        {
+            if (elapsedTime >= moveTime)
+            {
+                elapsedTime = 0;
+                for (int i = 0; i < stretchers.Count; i++)
+                {
+                    stretchers[i].Move(1);
+                }
+                currentState = State.pause;
+            }
+            else
+            {
+                for(int i = 0; i < stretchers.Count; i++)
+                {
+                    stretchers[i].Move(elapsedTime / moveTime);
+                }
+            }
+        }
+        else
+        {
+            if(elapsedTime >= pauseTime)
+            {
+                elapsedTime = 0;
+                currentState = State.moving;
+                for (int i = 0; i < stretchers.Count; i++)
+                {
+                    stretchers[i].Play();
+                    stretchers[i].stretched = !stretchers[i].stretched;
+                }
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        currentState = State.pause;
+        elapsedTime = 0;
+    }
+}
