ab004496ff88b2dec9e52170e2bf64192a6c2146
tasel220 <345t@naver.com>
2021-06-29 17:06:48 +0900

    바람 임시 이펙트 추가

--- Assets/Scripts/WindCtrl.cs
+++ Assets/Scripts/WindCtrl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WindCtrl : MonoBehaviour
 {
@@ -15,15 +16,21 @@ public class WindCtrl : MonoBehaviour
     private bool blowing;
     private float elapsedTime;
 
+    public Image windArrow;
+
     private void Start()
     {
         region = CameraController.instance.WhichRegion(transform.position.x, transform.position.y);
+        windArrow = GameObject.Find("windArrow").GetComponent<Image>();
     }
 
+    private bool vfxFadeOut = false;
     private void Update()
     {
         if (region != CameraController.instance.regionNum)
         {
+            if (blowing)
+                windArrow.color = Color.clear;
             return;
         }
         //Debug.Log("Player at " + CameraController.instance.regionNum);
@@ -36,21 +43,49 @@ public class WindCtrl : MonoBehaviour
                 elapsedTime = 0;
                 blowing = false;
                 source.Stop();
+                StartCoroutine(WindFade(false));
+
             }
         }
         else
         {
             if(elapsedTime >= stopTime)
             {
+                vfxFadeOut = false;
                 PlayerMovement_Kinematic.instance.windVelocity = new Vector2((direction == Direction.Right ? 1 : -1) * speed, 0);
                 elapsedTime = 0;
                 blowing = true;
                 source.Play();
             }
+
+            else if(elapsedTime >= stopTime - 0.5f)
+            {
+                if(!vfxFadeOut)
+                {
+                    vfxFadeOut = true;
+                    StartCoroutine(WindFade(true));
+                }
+                
+            }
+            //else
+            //{
+            //    windArrow.color = Color.clear;
+            //}
         }
     }
     
-    
+    private IEnumerator WindFade(bool fadeIn)
+    {
+        windArrow.transform.rotation = direction == Direction.Left ? Quaternion.Euler(0,0,180) : Quaternion.identity;
+        windArrow.color = new Color(1, 1, 1, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        windArrow.color = new Color(1, 1, 1, fadeIn ? 1 : 0);
+    }
+
+    public void StopWindByRegionChange()
+    {
+
+    }
 
     public void Reset()
     {
